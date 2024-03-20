4f4e27cc75537e2daeff574a579c2c05d42cf79a
laikaiyi <163814074+laikaiyi@users.noreply.github.com>
2024-03-21 02:51:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def GPT_response(text):
 
 
 # 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
